2dea1f7ff5332ec1d42f65e3de4123bf5e7c9abd
marcusah <makasuh@gmail.com>
2016-10-28 14:51:57 -0600

    Add files via upload

--- Light_indicator/__init__.py
+++ Light_indicator/__init__.py
@@ -35,14 +35,15 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.graphics.vertex_instructions import Ellipse, Rectangle
 from kivy.graphics.context_instructions import Color
-import os,inspect
+from functools import partial
+import os,inspect,sys
 
 class DummyClass: 
 	pass
 
 class Light(Widget):
 	Builder.load_file('light.kv')
-
+	light_color = ListProperty([1,0,0,1])
 class Light_indicator(Widget):
 	'''
 	Light_indicator class
@@ -58,79 +59,62 @@ class Light_indicator(Widget):
 	color = StringProperty("")# name of color to refrence the dictionary.
 	
 	off_color = color_dictionary['off']# the off color of the light.
-    	_light = Light()
 
-
+	
 
 	def __init__(self, **kwargs):
 		super(Light_indicator, self).__init__(**kwargs)
 		self._setting = Scatter(
 			size=(self.size_setting, self.size_setting),
 			do_rotate=False, 
-			do_scale=False,
+			do_scale=True,
 			do_translation=False
 			)
+		self._light = Light(light_color = self.get_color())
+		self._light.pos = (100,self._setting.center_y)
 		_img_setting = Image(source=self.file_setting, size=(self.size_setting, 
 			self.size_setting))
-		_light = Light()
-		_light.pos = self._setting.center
+
 		self._setting.add_widget(_img_setting)
-		self._setting.add_widget(_light)
 		self.add_widget(self._setting)
+		self.add_widget(self._light)
 		self.bind(pos=self._update)
 		self.bind(size=self._update)
+		Clock.schedule_interval(self.my_schedule, 0.1)
+		
 
 	def _update(self, *args):
 		
 		self._setting.pos = self.pos
-		self._light.pos = self._setting.center
-		self._light.Color = 1,0,0,1 #self.change_color()
+		self._setting.size = self.size
+		self._light.pos = self._setting.center#(100,self._setting.center_y)
+		self._light.light_color = self.get_color()
+#self.get_color()
 		
-	def change_color(self):
-		
+	def get_color(self):
+		color_rgba=self.off_color
 		if self.bol == True:
 			try:
 				color_rgba = self.color_dictionary[self.color]
-				print color_rgba
-				print self.color_dictionary[str(self.color)]
+
 			except:
-				print 'Your color doesn\'t exist in the dictionary. Add it to the __init__.py file to use that color.  '
+				print 'ERROR: Your color doesn\'t exist in the dictionary. Add it to the __init__.py file to use that color.  '
+				sys.exit()
 		else:
 			color_rgba = self.off_color
-			print color_rgba
+			
+		
+		
 		return color_rgba
 
-	def turn_on_off(self):
-		if self.bol ==False :
-			self.bol = True
-			self.change_color()
-		else:
-			self.bol = False
-			self.change_color()		
-class LightApp(App):
-# Function that turns on or off the light
 
 	def turn_on_off(self):
-		if self.bol ==False :
+		if self.bol == False :
 			self.bol = True
-			self.change_color()
+			self.get_color()
 		else:
 			self.bol = False
-			self.change_color()
-        def build(self):
-			from kivy.clock import Clock
-			from functools import partial
-
-				
-
-			mylight = Light_indicator()
-			box = BoxLayout(orientation='horizontal', spacing=5, padding=5)
-			Clock.schedule_once(turn_on_off())
-			box.add_widget(mylight)
-
-
-			return box
-            
-if __name__ == '__main__':
-    LightApp().run()
+			self.get_color() 
+	def my_schedule(self,dt):
+		self._light.light_color = self.get_color()	
 
